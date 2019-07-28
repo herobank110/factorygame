0c1ffe0e6eecb8adefceb93ea8a7985167f2b628
David <dkanekanian1@gmail.com>
2019-07-28 21:31:16 +0100

    Add blueprint file

---
+++ factorygame/core/blueprint.py
@@ -0,0 +1,26 @@
+from tkinter import Canvas
+from uuid import uuid4
+from factorygame.utils.loc import Loc
+
+class DrawableObject(object):
+    """Base class for all blueprint drawable objects."""
+
+    def __init__(self, owner, location=None):
+        """Initialiase drawable object with default properties.""" 
+
+        ## Blueprint graph this object is in.
+        self.owner = owner
+
+        ## 2D blueprint location with depth as Z.
+        self.location = Loc(0.0, 0.0, 0)
+
+        ## Random, unique ID for this drawable object.
+        self.unique_id = uuid4()
+
+        
+    def clear(self):
+        """Called every frame before drawing to clear current drawn representation."""
+
+    def draw(self):
+        """Called every frame to create the drawn representation."""
+        pass
