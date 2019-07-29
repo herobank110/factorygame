066e98746e4f180daaa652ae53ef141a2da31780
David <dkanekanian1@gmail.com>
2019-07-29 11:32:52 +0100

    Set location from parameters

--- factorygame/core/blueprint.py
+++ factorygame/core/blueprint.py
@@ -6,17 +6,23 @@ class DrawableObject(object):
     """Base class for all blueprint drawable objects."""
 
     def __init__(self, owner, location=None):
-        """Initialiase drawable object with default properties.""" 
+        """Initialiase drawable object with canvas OWNER at LOCATION."""
 
         ## Blueprint graph this object is in.
         self.owner = owner
 
         ## 2D blueprint location with depth as Z.
-        self.location = Loc(0.0, 0.0, 0)
+        if location is None:
+            self.location = Loc(0.0, 0.0, 0)
+        else:
+            # Use provided location.
+            self.location = Loc(location)
+            if len(location) < 3:
+                # Use 0 depth if depth not provided.
+                self.location.append(0)
 
         ## Random, unique ID for this drawable object.
         self.unique_id = uuid4()
-
         
     def clear(self):
         """Called every frame before drawing to clear current drawn representation."""
