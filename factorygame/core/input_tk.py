0703e340056cab3ec32169439e93ab3e5c7b4cce
David <dkanekanian1@gmail.com>
2019-09-26 18:36:09 +0100

    Add bind_all for input from all tkinter widgets

--- factorygame/core/input_tk.py
+++ factorygame/core/input_tk.py
@@ -14,17 +14,17 @@ class TkInputHandler(GUIInputHandler):
         """
 
         # Bind mouse events.
-        in_widget.bind("<ButtonPress-1>", lambda e: self.register_key_press(
+        in_widget.bind_all("<ButtonPress-1>", lambda e: self.register_key_press(
             EKeys.LeftMouseButton, EInputEvent.IE_PRESSED))
-        in_widget.bind("<ButtonRelease-1>", lambda e: self.register_key_press(
+        in_widget.bind_all("<ButtonRelease-1>", lambda e: self.register_key_press(
             EKeys.LeftMouseButton, EInputEvent.IE_RELEASED))
 
-        in_widget.bind("<ButtonPress-2>", lambda e: self.register_key_press(
+        in_widget.bind_all("<ButtonPress-2>", lambda e: self.register_key_press(
             EKeys.MiddleMouseButton, EInputEvent.IE_PRESSED))
-        in_widget.bind("<ButtonRelease-2>", lambda e: self.register_key_press(
+        in_widget.bind_all("<ButtonRelease-2>", lambda e: self.register_key_press(
             EKeys.MiddleMouseButton, EInputEvent.IE_RELEASED))
 
-        in_widget.bind("<ButtonPress-3>", lambda e: self.register_key_press(
+        in_widget.bind_all("<ButtonPress-3>", lambda e: self.register_key_press(
             EKeys.RightMouseButton, EInputEvent.IE_PRESSED))
-        in_widget.bind("<ButtonRelease-3>", lambda e: self.register_key_press(
+        in_widget.bind_all("<ButtonRelease-3>", lambda e: self.register_key_press(
             EKeys.RightMouseButton, EInputEvent.IE_RELEASED))
