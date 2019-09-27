c3eaa194c1656529019aafb2979cd0b67f9c983d
David <dkanekanian1@gmail.com>
2019-09-27 11:56:35 +0000

    Insert correct sys path at module start

--- main.py
+++ main.py
@@ -2,6 +2,17 @@
 Default program start for FactoryGame.
 """
 
+import sys
+
+try:
+    # Py 3.6
+    py_36_path, _ = sys.argv[0].split("/")
+except ValueError:
+    # Py 3.2
+    sys.path.insert(0, "\\".join(sys.argv[0].split("\\")[:-1]))
+else:
+    sys.path.insert(0, py_36_path)
+
 from factorygame.factory_engine import FactoryEngine
 from factorygame.utils.gameplay import GameplayUtilities
 
