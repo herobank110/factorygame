bfb44db2e582e2360b6c202375647cb6e0b121d3
David <dkanekanian1@gmail.com>
2019-10-12 18:42:22 +0100

    Add view to canvas override for HighwayWorld

--- spookygame/core/world.py
+++ spookygame/core/world.py
@@ -1,5 +1,5 @@
 from factorygame.core.blueprint import WorldGraph, GridGismo
-
+from factorygame import Loc, MathStat
 
 class HighwayWorld(WorldGraph):
     def __init__(self):
@@ -10,3 +10,29 @@ class HighwayWorld(WorldGraph):
 
         # Spawn the grid lines actor to show grid lines.
         self.spawn_actor(GridGismo, (0, 0))
+
+    def view_to_canvas(self, in_coords, clamp_to_viewport=False):
+        """
+        Return viewport coordinates in canvas coordinates as a Loc.
+        
+        :param in_coords: Viewport coordinates as a Loc.
+
+        :param clamp_to_viewport: Whether to clamp coordinates to valid
+        canvas coordinates.
+
+        :return: Canvas coordinates converted from in_coords.
+        """
+        canvas_dim = self.get_canvas_dim()
+        tr, bl = self.get_view_coords()
+
+        if clamp_to_viewport:
+            coords = Loc(
+            MathStat.map_range_clamped(in_coords.x, bl.x, tr.x, 0, canvas_dim.x),
+            MathStat.map_range_clamped(in_coords.y, bl.y, tr.y, canvas_dim.y, 0))
+
+        else:
+            coords = Loc(
+            MathStat.map_range(in_coords.x, bl.x, tr.x, 0, canvas_dim.x),
+            MathStat.map_range(in_coords.y, bl.y, tr.y, canvas_dim.y, 0))
+
+        return coords
