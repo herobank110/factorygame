8b3bb43acbfb7a309814c2a9eebd83fb42ed5cce
David <dkanekanian1@gmail.com>
2019-10-18 16:15:11 +0100

    Fix default engine formatting and imports

--- factorygame/factory_engine.py
+++ factorygame/factory_engine.py
@@ -1,9 +1,9 @@
-from factorygame.core.engine_base import GameEngine
+from factorygame import GameEngine, GameplayStatics
 from factorygame.core.blueprint import WorldGraph, GridGismo
-from factorygame.utils.gameplay import GameplayStatics
 
 class FactoryEngine(GameEngine):
     """Game engine class for factories."""
+
     def __init__(self):
 
         # Set default properties.
@@ -11,5 +11,5 @@ class FactoryEngine(GameEngine):
         self._frame_rate        = 90
         self._starting_world    = WorldGraph
 
-    def begin_play(self):        
+    def begin_play(self):
         GameplayStatics.world.spawn_actor(GridGismo, (0, 0))
