c81ddd10d876cef128fb5045f3921e380221f973
David <dkanekanian1@gmail.com>
2019-12-09 13:33:34 +0000

    Add neuron node network class

--- brainsim/core/world.py
+++ brainsim/core/world.py
@@ -3,7 +3,7 @@ from tkinter import Canvas
 from factorygame import FColor, GameplayStatics, Loc
 from factorygame.core.blueprint import WorldGraph, GridGismo
 from factorygame.core.input_base import EInputEvent
-from brainsim.neuron_node.nodes import NeuronNodeBase
+from brainsim.neuron_node.nodes import NeuronNodeBase, NeuronNodeNetwork
 
 
 class BrainWorld(WorldGraph):
@@ -14,16 +14,27 @@ class BrainWorld(WorldGraph):
         super().begin_play()
         self.config(bg=FColor(45, 23, 12).to_hex())
 
+        # Spawn initial world actors.
+
         grid = self.deferred_spawn_actor(GridGismo, (0, 0))
         grid.grid_line_color = FColor(45, 45, 17)
         grid.grid_text_color = FColor(145, 145, 117)
         grid.origin_line_color = FColor(45, 45, 17)
         self.finish_deferred_spawn_actor(grid)
 
+        # self.default_node_network = self.spawn_actor(
+        #     NeuronNodeNetwork, Loc(0, 0))
+        self.spawn_actor(NeuronNodeNetwork, Loc(0, 0))
+
+        # Setup input bindings.
+
         input_comp = GameplayStatics.game_engine.input_mappings
         input_comp.bind_action("AddNode", EInputEvent.PRESSED, self.on_add_node)
 
     def on_add_node(self):
+        if self.default_node_network is None:
+            return
+
         canvas = super(Canvas, self)
         mouse_position = Loc(
             canvas.winfo_pointerx() - canvas.winfo_rootx(),
@@ -32,4 +43,4 @@ class BrainWorld(WorldGraph):
         print("Adding node at %s" % mouse_position)
 
         spawn_position = self.canvas_to_view(mouse_position)
-        self.spawn_actor(NeuronNodeBase, spawn_position)
+        self.default_node_network.add_node(spawn_position)
--- brainsim/neuron_node/nodes.py
+++ brainsim/neuron_node/nodes.py
@@ -1,6 +1,51 @@
-from factorygame import FColor
+from factorygame import FColor, Actor
 from factorygame.core.blueprint import PolygonNode, GeomHelper
 
+
+class NeuronNodeNetwork(Actor):
+    """Represent a group of connected neurons in the synapse.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.primary_actor_tick.can_ever_tick = False
+
+        ## Default node class when adding nodes to the network.
+        self.default_node_class = NeuronNodeBase
+
+        ## Node currently being connected from.
+        self.held_node = None
+
+        ## Node currently being connected to.
+        self.hovered_node = None
+
+        ## All nodes in this group.
+        self.nodes = []
+
+    def add_node(self, location, node_class=None):
+        """Add a new node to the network.
+
+        :param location: (int) Location of the new node, in world
+        coordinates.
+
+        :param node_class: (NeuronNodeBase) Class of node to spawn. If
+        omitted, the default node class will be used.
+
+        :return: (node_class) Newly spawned node.
+        """
+
+        if node_class is None:
+            node_class = self.default_node_class
+
+        new_node = self.world.deferred_spawn_actor(node_class, location)
+
+        # Set references.
+        new_node.network = self
+        self.nodes.append(new_node)
+
+        self.world.finish_deferred_spawn_actor(new_node)
+        return new_node
 
 class NeuronNodeBase(PolygonNode):
     """Base class for neuron nodes.
