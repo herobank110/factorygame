b22868d7cdcedffe7633e52a44956a6865e005de
David <dkanekanian1@gmail.com>
2019-12-12 21:27:51 +0000

    Add reset polygon button

--- brainsim/core/world.py
+++ brainsim/core/world.py
@@ -1,4 +1,4 @@
-from tkinter import Canvas
+from tkinter import Canvas, Frame
 from tkinter.ttk import Button
 
 from factorygame import FColor, GameplayStatics, Loc
@@ -29,8 +29,16 @@ class BrainWorld(WorldGraph):
 
         self.hud = self.spawn_actor(BrainWorldHud, Loc(0, 0))
 
-        my_button = Button(self, text="Save", command=self.on_save_polygon)
-        self.create_window(Loc(20, 20), window=my_button, anchor="nw")
+        button_frame = Frame(self)
+        Button(
+            button_frame, text="Save",
+            command=self.on_save_polygon
+            ).pack()
+        Button(
+            button_frame, text="Reset",
+            command=self.on_reset_polygon
+            ).pack()
+        self.create_window(Loc(20, 20), window=button_frame, anchor="nw")
 
         # Setup input bindings.
 
@@ -55,11 +63,19 @@ class BrainWorld(WorldGraph):
             "Loc(%s, %s)" % (node.location.x, node.location.y)
             for node in self.default_node_network.nodes]
         coords_as_str = str(polygon_coords).replace("'", "")
+        self.clipboard_clear()
         self.clipboard_append(coords_as_str)
 
-        msg = ("Generating Polygon Coordinates".center(60) + "\n"
+        msg = ("Generating Polygon Coordinates".center(60, "-") + "\n"
                + "\n"
                + "Coords: %s" % coords_as_str[:40].replace("\n", " ") + "..." + "\n"
                + "Copied to clipboard")
 
         print(msg)
+
+    def on_reset_polygon(self):
+        for node in self.default_node_network.nodes:
+            self.destroy_actor(node)
+        self.default_node_network.nodes = []
+        self.default_node_network.held_node = None
+        self.default_node_network.hovered_node = None
