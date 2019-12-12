05a41fe62e9eea312601b538ec766f77d64eb0f5
David <dkanekanian1@gmail.com>
2019-12-12 13:29:58 +0000

    Change colors to lighter

--- brainsim/core/world.py
+++ brainsim/core/world.py
@@ -13,14 +13,14 @@ class BrainWorld(WorldGraph):
 
     def begin_play(self):
         super().begin_play()
-        self.config(bg=FColor(45, 23, 12).to_hex())
+        self.config(bg=FColor(210).to_hex())
 
         # Spawn initial world actors.
 
         grid = self.deferred_spawn_actor(GridGismo, (0, 0))
-        grid.grid_line_color = FColor(45, 45, 17)
-        grid.grid_text_color = FColor(145, 145, 117)
-        grid.origin_line_color = FColor(45, 45, 17)
+        grid.grid_line_color = FColor(160)
+        grid.grid_text_color = FColor(160)
+        grid.origin_line_color = FColor(160)
         self.finish_deferred_spawn_actor(grid)
 
         self.default_node_network = self.spawn_actor(
